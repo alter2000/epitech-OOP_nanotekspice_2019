aea74aff8a8574ba64e63b7dbdc6c1c707cbd762
Jor Bashllari <altnate2000@gmail.com>
2020-03-29 23:42:35 +0100

    move builder config

--- Builders.cpp
+++ Builders.cpp
@@ -7,61 +7,16 @@
 
 #include <algorithm>
 #include "Builders.hpp"
-#include "Components/Components.hpp"
 #include "Errors.hpp"
 
 namespace nts
 {
     std::function<ct(std::string)> mkComponent(ctType t)
     {
+        // TODO: loop std::pairs (str, cons)
         auto f = std::find(CPTS.begin(), CPTS.end(), t);
-        if (*f == "")
-            throw ComponentError("Unknown component type: " + t);
-        if (*f == std::string("input"))
-            return [](ctName n) { return ct(new Input(n)); };
-        if (*f == std::string("output"))
-            return [](ctName n) { return ct(new Output(n)); };
-        if (*f == std::string("true"))
-            return [](ctName n) { return ct(new True(n)); };
-        if (*f == std::string("false"))
-            return [](ctName n) { return ct(new False(n)); };
-        if (*f == std::string("clock"))
-            return [](ctName n) { return ct(new Clock(n)); };
-        if (*f == std::string("4001"))
-            return [](ctName n) { return ct(new C4001(n)); };
-        // if (*f == std::string("4008"))
-        //     return [](ctName n) { return ct(new C4008(n)); };
-        // if (*f == std::string("4011"))
-        //     return [](ctName n) { return ct(new C4011(n)); };
-        // if (*f == std::string("4013"))
-        //     return [](ctName n) { return ct(new C4013(n)); };
-        // if (*f == std::string("4017"))
-        //     return [](ctName n) { return ct(new C4017(n)); };
-        // if (*f == std::string("4030"))
-        //     return [](ctName n) { return ct(new C4030(n)); };
-        // if (*f == std::string("4040"))
-        //     return [](ctName n) { return ct(new C4040(n)); };
-        // if (*f == std::string("4069"))
-        //     return [](ctName n) { return ct(new C4069(n)); };
-        // if (*f == std::string("4071"))
-        //     return [](ctName n) { return ct(new C4071(n)); };
-        // if (*f == std::string("4081"))
-        //     return [](ctName n) { return ct(new C4081(n)); };
-        // if (*f == std::string("4094"))
-        //     return [](ctName n) { return ct(new C4094(n)); };
-        // if (*f == std::string("4514"))
-        //     return [](ctName n) { return ct(new C4514(n)); };
-        // if (*f == std::string("4801"))
-        //     return [](ctName n) { return ct(new C4801(n)); };
-        // if (*f == std::string("2716")) {
-        //     // TODO: add rom?
-        //     return [](ctName n) { return ct(new C2716(n)); };
-        // }
-        return [f] (ctName) {
-            std::cerr << "Unimplemented: " << *f << std::endl;
-            exit(84);
-            throw ComponentError("Unimplemented: " + *f);
-            return ct();
-        };
+        if (f->first == "")
+            throw ComponentError("Cannot add component of type " + t);
+        return f->second;
     }
 }
--- Builders.hpp
+++ Builders.hpp
@@ -10,15 +10,36 @@
 
 #include <functional>
 #include "IComponent.hpp"
+#include "Components/Components.hpp"
 #include "Primitives/Primitives.hpp"
 
 namespace nts
 {
+    using ctPair = std::pair<std::string, std::function<ct(std::string)>>;
+
     std::function<ct(std::string)> mkComponent(ctType t);
-    static const std::vector<std::string> CPTS = {
-        "input" , "output" , "true" , "false" , "clock" , "4001" , "4008" ,
-        "4011"  , "4013"   , "4017" , "4030"  , "4040"  , "4069" , "4071" ,
-        "4081"  , "4094"   , "4514" , "4801"  , "2716"  , ""
+    static const std::vector<ctPair>
+        CPTS = {
+            ctPair("input",  [] (ctName n) { return ct(new Input(n)); }),
+            ctPair("output", [] (ctName n) { return ct(new Output(n)); }),
+            ctPair("true",   [] (ctName n) { return ct(new True(n)); }),
+            ctPair("false",  [] (ctName n) { return ct(new False(n)); }),
+            ctPair("clock",  [] (ctName n) { return ct(new Clock(n)); }),
+            ctPair("4001",   [] (ctName n) { return ct(new C4001(n)); }),
+            // ctPair("4008",   [] (ctName n) { return ct(new C4008(n)); }),
+            // ctPair("4011",   [] (ctName n) { return ct(new C4011(n)); }),
+            // ctPair("4013",   [] (ctName n) { return ct(new C4013(n)); }),
+            // ctPair("4017",   [] (ctName n) { return ct(new C4017(n)); }),
+            // ctPair("4030",   [] (ctName n) { return ct(new C4030(n)); }),
+            // ctPair("4040",   [] (ctName n) { return ct(new C4040(n)); }),
+            // ctPair("4069",   [] (ctName n) { return ct(new C4069(n)); }),
+            // ctPair("4071",   [] (ctName n) { return ct(new C4071(n)); }),
+            // ctPair("4081",   [] (ctName n) { return ct(new C4081(n)); }),
+            // ctPair("4094",   [] (ctName n) { return ct(new C4094(n)); }),
+            // ctPair("4514",   [] (ctName n) { return ct(new C4514(n)); }),
+            // ctPair("4801",   [] (ctName n) { return ct(new C4801(n)); }),
+            // ctPair("2716",   [] (ctName n) { return ct(new C2716(n)); }),
+            ctPair("", [] (ctName) { return nullptr; }),
 };
 
 }
